53d7413c2cd85263b6e05cfae635449f7fe48743
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-08-28 15:08:13 +0100

    Updates

---
+++ precache-manifest.da6e04fa08e97f5a4607f843d013c539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062d9198d7b92e027efc7289125bdd5d",
+    "url": "/Rect-personal-website/index.html"
+  },
+  {
+    "revision": "1023912d88137f7ae2d6",
+    "url": "/Rect-personal-website/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "d6a21b2490cf0a3a3d4a",
+    "url": "/Rect-personal-website/static/css/main.f641b9d7.chunk.css"
+  },
+  {
+    "revision": "1023912d88137f7ae2d6",
+    "url": "/Rect-personal-website/static/js/2.ae321a6a.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Rect-personal-website/static/js/2.ae321a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a21b2490cf0a3a3d4a",
+    "url": "/Rect-personal-website/static/js/main.2187f328.chunk.js"
+  },
+  {
+    "revision": "766fc4652b2e4aefbebd",
+    "url": "/Rect-personal-website/static/js/runtime-main.8b99cbaa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rect-personal-website/precache-manifest.9390ce28fe61974899ac1663a42ec673.js"
+  "/Rect-personal-website/precache-manifest.da6e04fa08e97f5a4607f843d013c539.js"
 );
 
 self.addEventListener('message', (event) => {
