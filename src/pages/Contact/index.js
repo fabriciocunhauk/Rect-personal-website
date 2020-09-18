160551fdae2385f606e99026b6051ef68a6f12e0
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-09-18 01:49:09 +0100

    Contact icons increase on hover

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -32,16 +32,16 @@ function Contact() {
 
                 <div className="social-links">
                     <a href="https://www.linkedin.com/in/fabricio-cunha-7b7392162/" rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-linkedin" arial-hidden="true" />
+                        <i className="fa fa-linkedin icon-increase" arial-hidden="true" />
                     </a>
                     <a href="https://github.com/fabriciocunhauk" rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-github" arial-hidden="true" />
+                        <i className="fa fa-github icon-increase" arial-hidden="true" />
                     </a>
                     <a href="https://www.facebook.com/fabricio.cunha.54?ref=bookmarks" rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-facebook" arial-hidden="true" />
+                        <i className="fa fa-facebook icon-increase" arial-hidden="true" />
                     </a>
                     <a href="https://www.instagram.com/fabriciocunhauk/?hl=pt" rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-instagram" arial-hidden="true" />
+                        <i className="fa fa-instagram icon-increase" arial-hidden="true" />
                     </a>
                 </div>
             </div>
