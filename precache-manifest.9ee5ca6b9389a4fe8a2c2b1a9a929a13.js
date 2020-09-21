8529cd2631716c5156d1911d3cc8615e10298e80
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-09-21 17:11:24 +0100

    Updates

--- precache-manifest.9ee5ca6b9389a4fe8a2c2b1a9a929a13.js
+++ precache-manifest.9ee5ca6b9389a4fe8a2c2b1a9a929a13.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ec36beeeae1c87c37666144ada0c2dd6",
+    "revision": "60139d8a7d0a828930dbf4faef0c4e6c",
     "url": "/Rect-personal-website/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Rect-personal-website/static/css/2.e4346227.chunk.css"
   },
   {
-    "revision": "65a60e8e67e59e9b2a6b",
-    "url": "/Rect-personal-website/static/css/main.d6d89774.chunk.css"
+    "revision": "2ef6135e8e43ed573cb0",
+    "url": "/Rect-personal-website/static/css/main.ed819c5e.chunk.css"
   },
   {
     "revision": "da3ca36a8cb2b48ba665",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Rect-personal-website/static/js/2.b70a3ceb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "65a60e8e67e59e9b2a6b",
-    "url": "/Rect-personal-website/static/js/main.2117c065.chunk.js"
+    "revision": "2ef6135e8e43ed573cb0",
+    "url": "/Rect-personal-website/static/js/main.57c02922.chunk.js"
   },
   {
     "revision": "766fc4652b2e4aefbebd",
